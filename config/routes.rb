98196fa0fae8b310ba4ddf4f5e67c82f677621eb
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-11-06 08:43:27 +0000

    6w 0243

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
 
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
 end
