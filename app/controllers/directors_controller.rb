d14efec49f063466341cab88359dd2fe02ba9481
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-11-06 09:38:12 +0000

    done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,9 +9,16 @@ class DirectorsController < ApplicationController
     matching_records = Director.where({ :id => the_id })
 
     @the_director = matching_records.at(0)
-    
+
     render({ :template => "director_templates/details" })
   end
 
+  def youngest
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def eldest
+    render({ :template => "director_templates/eldest" })
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+
   get("/directors", { :controller => "directors", :action => "index" })
 
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
