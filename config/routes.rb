440952cb2f779439249729ec43e06a8ea8a6038b
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-11-06 09:06:47 +0000

    6w 0306

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
 
+  get("/actors", { :controller => "actors", :action => "index" })
+
+  get("/actors/:the_id", { :controller => "actors", :action => "show" })
+
   get("/movies", { :controller => "movies", :action => "index" })
 
   get("/movies/:the_id", { :controller => "movies", :action => "show" })
